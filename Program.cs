2c2b204b6be69f23e1aebd7970e1bece26a76b67
Vijay Velaga <vijay.velaga@gmail.com>
2018-07-16 19:18:52 -0700

    first

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectEuler
+{
+    class Program
+    {
+       public static int count;
+        static void Main(string[] args)
+        {
+            for(int x=1; x < 10; x++)
+            {
+                if(x%3==0 || x % 5 == 0)
+                {
+                    count += x;
+                }
+
+            }
+            Console.WriteLine(count);
+            Console.Read();
+        }
+    }
+}
